9b3838bcd5ee971b1f2d3ef9e70eb90f3099a7c7
ww <wangwei@xiangha.com>
2019-02-15 12:34:45 +0800

    init

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ps -e|grep brook | awk '{print $1}'|xargs kill -9
-nohup /root/brook socks5 -l :9999 -i 47.91.221.14 >/dev/null 2>log &
+nohup /root/brook socks5 -l :9999 -i 47.75.11.103 >/dev/null 2>log &
 nohup /root/brook ssserver -l :8989 -p 123456 >/dev/null 2>log &
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup /root/brook socks5 -l :9999 -i 47.91.221.14 >/dev/null 2>log &
+nohup /root/brook socks5 -l :9999 -i 47.75.11.103 >/dev/null 2>log &
 nohup /root/brook ssserver -l :8989 -p 123456 >/dev/null 2>log &
