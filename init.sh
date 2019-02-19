5c1ae58aea6fe1e60fd9c68c056d4fe46d43525d
ww <wangwei@xiangha.com>
2019-02-19 10:02:42 +0800

    init

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /root
 wget https://github.com/txthinking/brook/releases/download/v20190205/brook
-wget https://raw.githubusercontent.com/qingfenghuohu/brook/master/cron.sh
+wget https://raw.githubusercontent.com/qingfenghuohu/brook/master/restart.sh
 wget https://raw.githubusercontent.com/qingfenghuohu/brook/master/start.sh
 chmod +x brook cron.sh start.sh
 sh /root/start.sh
