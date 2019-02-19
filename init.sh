76f8ed12cf6788141bc207ae0c78821eff9fdcfa
ww <wangwei@xiangha.com>
2019-02-19 10:16:57 +0800

    init

--- init.sh
+++ init.sh
@@ -5,3 +5,4 @@ wget https://raw.githubusercontent.com/qingfenghuohu/brook/master/restart.sh
 wget https://raw.githubusercontent.com/qingfenghuohu/brook/master/start.sh
 chmod +x brook cron.sh start.sh
 sh /root/start.sh
+echo "sh /root/start.sh" >> /etc/rc.d/rc.local
