a6466b349704bd8d9af967b5ece8ab975d5a7733
ww <wangwei@xiangha.com>
2019-03-07 11:18:18 +0800

    init

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ cd /root
 wget https://github.com/txthinking/brook/releases/download/v20190205/brook
 wget https://raw.githubusercontent.com/qingfenghuohu/brook/master/restart.sh
 wget https://raw.githubusercontent.com/qingfenghuohu/brook/master/start.sh
-chmod +x brook cron.sh start.sh
+chmod +x brook restart.sh start.sh
 path=`pwd`
 sh ./start.sh
 echo "sh ${path}/start.sh" >> /etc/rc.d/rc.local
