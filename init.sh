35d30b7f8fd3d51419c4a0e91c48efda18533b7d
ww <wangwei@xiangha.com>
2019-03-07 11:23:04 +0800

    init

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd /root
 wget https://github.com/txthinking/brook/releases/download/v20190205/brook
 wget https://raw.githubusercontent.com/qingfenghuohu/brook/master/restart.sh
 wget https://raw.githubusercontent.com/qingfenghuohu/brook/master/start.sh
